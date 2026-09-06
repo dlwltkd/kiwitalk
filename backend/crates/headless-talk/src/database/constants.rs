e29983be00973e41501bfb9e9dc053b11b47a0b7
dlwltkd <birkensilitch@gmail.com>
2026-09-06 12:53:32 +0900

    refactor: update backend/crates/headless-talk/src/database/constants.rs

--- backend/crates/headless-talk/src/database/constants.rs
+++ backend/crates/headless-talk/src/database/constants.rs
@@ -1,6 +1,10 @@
-pub const INIT_SQL: &str = "
+pub const DATABASE_INIT_SQL: &str = "
+PRAGMA busy_timeout = 5000;
 PRAGMA journal_mode = WAL;
+";
+
+pub const CONNECTION_INIT_SQL: &str = "
+PRAGMA busy_timeout = 5000;
 PRAGMA synchronous = NORMAL;
 PRAGMA wal_autocheckpoint = 8192;
-PRAGMA wal_checkpoint(TRUNCATE);
 ";
