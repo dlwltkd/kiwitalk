97cee9c611a1f68831fb1f2f7b86283c16d8bf06
dlwltkd <birkensilitch@gmail.com>
2026-09-06 12:53:29 +0900

    refactor: update backend/bin/app/build.rs

--- backend/bin/app/build.rs
+++ backend/bin/app/build.rs
@@ -1,3 +1,45 @@
 fn main() {
-    tauri_build::build()
+    use tauri_build::{Attributes, DefaultPermissionRule, InlinedPlugin};
+
+    fn plugin(commands: &'static [&'static str]) -> InlinedPlugin {
+        InlinedPlugin::new()
+            .commands(commands)
+            .default_permission(DefaultPermissionRule::AllowAllCommands)
+    }
+
+    tauri_build::try_build(Attributes::new().plugins([
+        (
+            "api",
+            plugin(&[
+                "logon",
+                "login",
+                "logout",
+                "auto_login",
+                "poll_android_registration",
+                "cancel_android_registration",
+                "default_login_form",
+                "me_profile",
+                "friend_profile",
+                "update_friends",
+            ]),
+        ),
+        (
+            "client",
+            plugin(&[
+                "created",
+                "create",
+                "destroy",
+                "next_event",
+                "channel_list",
+                "load_channel",
+                "channel_sync_history",
+                "channel_send_text",
+                "channel_load_chat",
+                "normal_channel_read_chat",
+            ]),
+        ),
+        ("configuration", plugin(&["load", "save"])),
+        ("system", plugin(&["get_device_locale", "get_device_name"])),
+    ]))
+    .expect("failed to build KiwiTalk's Tauri configuration");
 }
