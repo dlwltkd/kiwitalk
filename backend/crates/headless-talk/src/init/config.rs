5e80d86ebbeb89d63a1dd1de0292b268845b0733
dlwltkd <birkensilitch@gmail.com>
2026-09-06 12:53:34 +0900

    refactor: update backend/crates/headless-talk/src/init/config.rs

--- backend/crates/headless-talk/src/init/config.rs
+++ backend/crates/headless-talk/src/init/config.rs
@@ -1,3 +1,5 @@
+use talk_loco_client::talk::session::login::ResponseType;
+
 #[derive(Debug, Clone, Copy)]
 pub struct ClientEnv<'a> {
     pub os: &'a str,
@@ -5,6 +7,13 @@ pub struct ClientEnv<'a> {
     pub net_type: NetworkType,
     pub mccmnc: &'a str,
     pub language: &'a str,
+    pub protocol_version: &'a str,
+    pub device_type: Option<i8>,
+    pub revision: Option<i32>,
+    pub include_pc_status: bool,
+    pub background: Option<bool>,
+    pub last_chat_id: Option<i64>,
+    pub login_response_type: ResponseType,
 }
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
