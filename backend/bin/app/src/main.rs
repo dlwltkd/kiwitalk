c1ed3afaf1190138bcd97ea1feaf8bc7e64c757a
dlwltkd <birkensilitch@gmail.com>
2026-09-06 12:53:29 +0900

    refactor: update backend/bin/app/src/main.rs

--- backend/bin/app/src/main.rs
+++ backend/bin/app/src/main.rs
@@ -5,15 +5,15 @@
 
 mod configuration;
 
-use kiwi_talk_system::get_system_info;
 use log::info;
 use tauri::{
-    api::dialog, AppHandle, CustomMenuItem, DeviceEventFilter, Manager, RunEvent, Runtime,
-    SystemTray, SystemTrayEvent, SystemTrayMenu, Window, WindowBuilder,
+    menu::{Menu, MenuItem},
+    tray::{MouseButton, MouseButtonState, TrayIconBuilder, TrayIconEvent},
+    AppHandle, DeviceEventFilter, Manager, RunEvent, Runtime, WebviewUrl, WebviewWindow,
+    WebviewWindowBuilder,
 };
-use tauri_plugin_log::LogTarget;
-use tauri_plugin_window_state::{StateFlags, WindowExt};
-use window_shadows::set_shadow;
+use tauri_plugin_dialog::DialogExt;
+use tauri_plugin_log::{Target, TargetKind};
 
 #[cfg(target_os = "macos")]
 use window_vibrancy::{apply_vibrancy, NSVisualEffectMaterial};
@@ -21,17 +21,17 @@ use window_vibrancy::{apply_vibrancy, NSVisualEffectMaterial};
 #[cfg(target_os = "windows")]
 use window_vibrancy::apply_acrylic;
 
-fn create_main_window<R: Runtime>(manager: &impl Manager<R>) -> anyhow::Result<Window<R>> {
+fn create_main_window<R: Runtime>(manager: &impl Manager<R>) -> anyhow::Result<WebviewWindow<R>> {
     let window_builder =
-        WindowBuilder::new(manager, "main", tauri::WindowUrl::App("index.html".into()))
+        WebviewWindowBuilder::new(manager, "main", WebviewUrl::App("index.html".into()))
             .inner_size(1280.0, 720.0)
+            .min_inner_size(640.0, 420.0)
             .resizable(true)
             .title("KiwiTalk")
             .decorations(false)
             .visible(false);
 
     #[cfg(target_os = "linux")]
-    // TODO: see https://github.com/tauri-apps/tao/issues/470 (original winit supports `window.set_blur(true)`)
     let window = window_builder.build()?;
     #[cfg(not(target_os = "linux"))]
     let window = window_builder.transparent(true).build()?;
@@ -48,40 +48,14 @@ fn create_main_window<R: Runtime>(manager: &impl Manager<R>) -> anyhow::Result<W
 
     #[cfg(target_os = "windows")]
     apply_acrylic(&window, Some((0, 0, 0, 125)))
-        .expect("Unsupported platform! 'apply_blur' is only supported on Windows");
-    set_shadow(&window, true).ok();
-    window.restore_state(StateFlags::all())?;
+        .expect("Unsupported platform! 'apply_acrylic' is only supported on Windows");
 
     Ok(window)
 }
 
-async fn init_plugin(handle: &AppHandle<impl Runtime>) -> anyhow::Result<()> {
-    handle.plugin(kiwi_talk_system::init(handle.path_resolver()).await?)?;
-
-    handle.plugin(
-        tauri_plugin_log::Builder::new()
-            .targets([
-                LogTarget::Folder(get_system_info().data_dir.join("logs")),
-                LogTarget::Stderr,
-                LogTarget::Webview,
-            ])
-            .level(
-                #[cfg(not(debug_assertions))]
-                log::LevelFilter::Info,
-                #[cfg(debug_assertions)]
-                log::LevelFilter::Trace,
-            )
-            .build(),
-    )?;
-    handle.plugin(tauri_plugin_window_state::Builder::default().build())?;
-    handle.plugin(tauri_plugin_single_instance::init(|app, argv, cwd| {
-        info!(
-            "app: {} argv: {argv:?} cwd: {cwd:?}",
-            &app.package_info().name
-        );
-    }))?;
-
-    handle.plugin(kiwi_talk_api::init().await)?;
+async fn init_app_plugins(handle: &AppHandle<impl Runtime>) -> anyhow::Result<()> {
+    handle.plugin(kiwi_talk_system::init(handle.path().clone()).await?)?;
+    handle.plugin(kiwi_talk_api::init().await?)?;
     handle.plugin(configuration::init_plugin("configuration").await?)?;
     handle.plugin(kiwi_talk_client::init("client").await?)?;
 
@@ -89,61 +63,97 @@ async fn init_plugin(handle: &AppHandle<impl Runtime>) -> anyhow::Result<()> {
 }
 
 fn show_window(app: &AppHandle<impl Runtime>) {
-    let main_window = if let Some(window) = app.get_window("main") {
+    let main_window = if let Some(window) = app.get_webview_window("main") {
         window
     } else {
         let window = create_main_window(app).unwrap();
-        window.restore_state(StateFlags::all()).unwrap();
         window.show().unwrap();
 
         window
     };
-    main_window.set_focus().unwrap();
+
+    main_window.unminimize().ok();
+    main_window.show().ok();
+    main_window.set_focus().ok();
 }
 
-fn on_tray_event(app: &AppHandle<impl Runtime>, event: SystemTrayEvent) {
-    match event {
-        SystemTrayEvent::LeftClick { .. } => {
-            show_window(app);
-        }
+fn create_tray<R: Runtime>(app: &AppHandle<R>) -> anyhow::Result<()> {
+    let show_item = MenuItem::with_id(app, "show", "Show Window", true, None::<&str>)?;
+    let quit_item = MenuItem::with_id(app, "quit", "Quit KiwiTalk", true, None::<&str>)?;
+    let menu = Menu::with_items(app, &[&show_item, &quit_item])?;
 
-        SystemTrayEvent::MenuItemClick { id, .. } if id == "show" => {
-            show_window(app);
-        }
+    let mut tray = TrayIconBuilder::new()
+        .menu(&menu)
+        .show_menu_on_left_click(false)
+        .on_menu_event(|app, event| match event.id().as_ref() {
+            "show" => show_window(app),
+            "quit" => app.exit(0),
+            _ => {}
+        })
+        .on_tray_icon_event(|tray, event| {
+            if let TrayIconEvent::Click {
+                button: MouseButton::Left,
+                button_state: MouseButtonState::Up,
+                ..
+            } = event
+            {
+                show_window(tray.app_handle());
+            }
+        });
 
-        SystemTrayEvent::MenuItemClick { id, .. } if id == "quit" => {
-            app.exit(0);
-        }
-
-        _ => {}
+    if let Some(icon) = app.default_window_icon() {
+        tray = tray.icon(icon.clone());
     }
+
+    tray.build(app)?;
+
+    Ok(())
 }
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
     tauri::async_runtime::set(tokio::runtime::Handle::current());
 
-    let show_item = CustomMenuItem::new("show", "Show Window"); // TODO tag for translation
-    let quit_item = CustomMenuItem::new("quit", "Quit KiwiTalk");
-
-    let tray_menu = SystemTrayMenu::new()
-        .add_item(show_item)
-        .add_item(quit_item);
-
-    let system_tray = SystemTray::new().with_menu(tray_menu);
+    let log_plugin = tauri_plugin_log::Builder::new()
+        .targets([
+            Target::new(TargetKind::LogDir { file_name: None }),
+            Target::new(TargetKind::Stderr),
+            Target::new(TargetKind::Webview),
+        ])
+        .level(if cfg!(debug_assertions) {
+            log::LevelFilter::Trace
+        } else {
+            log::LevelFilter::Info
+        })
+        .build();
 
     let app = tauri::Builder::default()
-        .system_tray(system_tray)
+        .plugin(tauri_plugin_single_instance::init(|app, argv, cwd| {
+            info!(
+                "app: {} argv: {argv:?} cwd: {cwd:?}",
+                app.package_info().name
+            );
+            show_window(app);
+        }))
+        .plugin(log_plugin)
+        .plugin(tauri_plugin_window_state::Builder::default().build())
+        .plugin(tauri_plugin_dialog::init())
+        .plugin(tauri_plugin_http::init())
+        .plugin(tauri_plugin_notification::init())
         .device_event_filter(DeviceEventFilter::Never)
-        .on_system_tray_event(on_tray_event)
         .build(tauri::generate_context!())?;
 
-    if let Err(err) = init_plugin(&app.handle()).await {
-        dialog::message::<tauri::Wry>(None, "KiwiTalk Startup Fatal Error", format!("{:?}", err));
+    if let Err(err) = init_app_plugins(app.handle()).await {
+        app.dialog()
+            .message(format!("{err:?}"))
+            .title("KiwiTalk Startup Fatal Error")
+            .blocking_show();
         app.run(|_, _| {});
 
         return Ok(());
     }
+
+    create_tray(app.handle())?;
 
     let main_window = create_main_window(&app)?;
     main_window.show()?;
@@ -154,8 +164,10 @@ async fn main() -> anyhow::Result<()> {
     }
 
     app.run(|_, event| {
-        if let RunEvent::ExitRequested { api, .. } = event {
-            api.prevent_exit();
+        if let RunEvent::ExitRequested { code, api, .. } = event {
+            if code.is_none() {
+                api.prevent_exit();
+            }
         }
     });
 
