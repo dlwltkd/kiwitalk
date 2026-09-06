92ae7144b8d74e8f3eee2ac50182a3636876c165
dlwltkd <birkensilitch@gmail.com>
2026-09-06 12:53:36 +0900

    refactor: update backend/crates/talk-api-internal/src/credential.rs

--- backend/crates/talk-api-internal/src/credential.rs
+++ backend/crates/talk-api-internal/src/credential.rs
@@ -1,5 +1,13 @@
-#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+use std::fmt;
+
+#[derive(Clone, Copy, PartialEq, Eq)]
 pub struct Credential<'a> {
     pub device_uuid: &'a str,
     pub access_token: &'a str,
+}
+
+impl fmt::Debug for Credential<'_> {
+    fn fmt(&self, formatter: &mut fmt::Formatter<'_>) -> fmt::Result {
+        formatter.write_str("Credential(REDACTED)")
+    }
 }
