d85a8799da29eef393220067a3263c5d59060361
dlwltkd <birkensilitch@gmail.com>
2026-09-06 12:53:39 +0900

    refactor: update frontend/src/api/config.ts

--- frontend/src/api/config.ts
+++ frontend/src/api/config.ts
@@ -1,10 +1,10 @@
-import { tauri } from '@tauri-apps/api';
+import { invoke } from '@tauri-apps/api/core';
 import { GlobalConfig } from './_types';
 
 export function loadConfig(): Promise<GlobalConfig> {
-  return tauri.invoke<GlobalConfig>('plugin:configuration|load');
+  return invoke<GlobalConfig>('plugin:configuration|load');
 }
 
 export function saveConfig(configuration: GlobalConfig): Promise<void> {
-  return tauri.invoke('plugin:configuration|save', { configuration });
+  return invoke('plugin:configuration|save', { configuration });
 }
