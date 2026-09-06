4e903edd630cd0d3d9e110c8c3ff403cadb0da3e
dlwltkd <birkensilitch@gmail.com>
2026-09-06 12:53:33 +0900

    refactor: update backend/crates/headless-talk/src/database/model/user/normal.rs

--- backend/crates/headless-talk/src/database/model/user/normal.rs
+++ backend/crates/headless-talk/src/database/model/user/normal.rs
@@ -35,23 +35,33 @@ impl<'a> NormalChannelUserRow<'a> {
 #[derive(Debug, AsChangeset, Clone, PartialEq, Eq)]
 #[diesel(table_name = normal_channel_user)]
 pub struct NormalChannelUserUpdate<'a> {
-    pub country_iso: &'a str,
+    pub country_iso: Option<&'a str>,
 
-    pub account_id: i64,
-    pub status_message: &'a str,
-    pub linked_services: &'a str,
+    pub account_id: Option<i64>,
+    pub status_message: Option<&'a str>,
+    pub linked_services: Option<&'a str>,
 
-    pub suspended: bool,
+    pub suspended: Option<bool>,
+}
+
+impl NormalChannelUserUpdate<'_> {
+    pub fn has_changes(&self) -> bool {
+        self.country_iso.is_some()
+            || self.account_id.is_some()
+            || self.status_message.is_some()
+            || self.linked_services.is_some()
+            || self.suspended.is_some()
+    }
 }
 
 impl<'a> From<&'a normal::user::User> for NormalChannelUserUpdate<'a> {
     fn from(user: &'a normal::user::User) -> Self {
         Self {
-            country_iso: &user.country_iso,
-            account_id: user.account_id,
-            status_message: &user.status_message,
-            linked_services: &user.linked_services,
-            suspended: user.suspended,
+            country_iso: user.country_iso_if_present(),
+            account_id: user.account_id_if_present(),
+            status_message: user.status_message_if_present(),
+            linked_services: user.linked_services_if_present(),
+            suspended: user.suspended_if_present(),
         }
     }
 }
